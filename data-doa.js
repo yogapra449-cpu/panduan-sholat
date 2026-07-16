f7836dee367b8602c61747576bc4ac3d767f059e
yoga prasetyawan <yogapra449@gmail.com>
2026-07-17 05:05:56 +0700

    feat: menyelesaikan fitur zikir dan doa dan memperbaiki seluruh fungsi

---
+++ data-doa.js
@@ -0,0 +1,34 @@
+const dataDoaDanZikir = {
+    zikir: [
+        {
+            nama: "Istighfar",
+            arab: "أَسْتَغْفِرُ اللهَ الْعَظِىْمَ",
+            latin: "Astaghfirullaahal 'adzhiim",
+            terjemah: "Aku memohon ampun kepada Allah Yang Maha Agung."
+        },
+        {
+            nama: "Tasbih (33x)",
+            arab: "سُبْحَانَ اللهِ",
+            latin: "Subhaanallah",
+            terjemah: "Maha Suci Allah."
+        },
+        {
+            nama: "Tahmid (33x)",
+            arab: "الْحَمْدُ لِلّٰهِ",
+            latin: "Alhamdulillaah",
+            terjemah: "Segala puji bagi Allah."
+        },
+        {
+            nama: "Takbir (33x)",
+            arab: "اللهُ أَكْبَرُ",
+            latin: "Allaahu akbar",
+            terjemah: "Allah Maha Besar."
+        }
+    ],
+    doaSetelahShalat: {
+        judul: "Doa Setelah Shalat Fardhu ",
+        arab: "اَلْحَمْدُ لِلّٰهِ رَبِّ الْعَالَمِيْنَ. حَمْدًا يُوَافِيْ نِعَمَهُ وَيُكَافِئُ مَزِيْدَهُ. يَا رَبَّنَا لَكَ الْحَمْدُ كَمَا يَنْبَغِيْ لِجَلَالِ وَجْهِكَ وَعَظِيْمِ سُلْطَانِكَ. اَللّٰهُمَّ صَلِّ وَسَلِّم *عَلَى سَيِّدِنَا مُحَمَّدٍ وَعَلَى آلِ سَيِّدِنَا مُحَمَّدٍ. اَللّٰهُمَّ إِنَّا نَسْأَلُكَ سَلَامَةً فِى الدِّيْنِ، وَعَافِيَةً فِى الْجَسَدِ، وَزِيَادَةً فِى الْعِلْمِ، وَبَرَكَةً فِى الرِّزْقِ، وَتَوْبَةً قَبْلَ الْمَوْتِ، وَرَحْمَةً عِنْدَ الْمَوْتِ، وَمَغْفِرَةً بَعْدَ الْمَوْتِ. اَللّٰهُمَّ هَوِّنْ عَلَيْنَا فِىْ سَكَرَاتِ الْمَوْتِ، وَالنَّجَاةَ مِنَ النَّارِ وَالْعَفْوَ عِنْدَ الْحِسَابِ. رَبَّنَا اغْفِرْ لَنَا وَلِوَالِدِيْنَا وَارْحَمْهُمْ كَمَا رَبَّوْنَا صِغَارًا. رَبَّنَا آتِنَا فِى الدُّنْيَا حَسَنَةً وَفِى الْآخِرَةِ حَسَنَةً وَقِنَا عَذَابَ النَّارِ. وَصَلَّى اللهُ عَلَى سَيِّدِنَا مُحَمَّدٍ وَعَلَى آلِهِ وَصَحْبِهِ وَسَلَّمَ، وَالْحَمْدُ لِلّٰهِ رَبِّ الْعَالَمِيْنَ.",
+        latin: "Alhamdu lillaahi rabbil 'aalamiin. Hamdan yuwaafii ni'amahuu wa yukaafï'u maziidah. Yaa rabbanaa lakal hamdu kamaa yanbaghii lijalaali wajhika wa 'adzhiimi sulthaanik. Allaahumma shalli wa sallim 'alaa sayyidinaa muhammadin wa 'alaa aali sayyidinaa muhammad. Allaahumma innaa nas'aluka salaamatan fid-diin, wa 'aafiyatan fil-jasad, wa ziyaadatan fil-'ilmi, wa barakatan fir-rizqi, wa taubatan qablal-maut, wa rahmatan 'indal-maut, wa maghfiratan ba'dal-maut. Allaahumma hawwin 'alainaa fii sakaraatil-maut, wan-najaata minan-naari wal-'afwa 'indal-hisaab. Rabbanaghfir lanaa wa liwaalidiinaa warhamhum kamaa rabbauunaa shigaaraa. Rabbanaa aatinaa fid-dunyaa hasanataw wa fil-aakhirati hasanataw wa qinaa 'adzaaban-naar. Wa shallallaahu 'alaa sayyidinaa muhammadin wa 'alaa aalihii wa shahbihii wa sallam, wal-hamdu lillaahi rabbil 'aalamiin.",
+        terjemah: "Segala puji bagi Allah, Tuhan semesta alam. Pujian yang sebanding dengan nikmat-nikmat-Nya dan menjamin tambahannya. Wahai Tuhan kami, bagi-Mu-lah segala puji, sebagaimana kelayakan keagungan zat-Mu dan kebesaran kekuasaan-Mu. Ya Allah, limpahkanlah rahmat dan keselamatan kepada junjungan kami Nabi Muhammad dan kepada keluarga Nabi Muhammad. Ya Allah, sesungguhnya kami memohon kepada-Mu keselamatan dalam agama, kesehatan jasmani, tambahan ilmu pengetahuan, keberkahan dalam rezeki, taubat sebelum mati, rahmat ketika mati, dan ampunan setelah mati. Ya Allah, mudahkanlah kami pada saat sakaratul maut, selamatkanlah kami dari api neraka, dan berikanlah kami ampunan pada saat hisab. Wahai Tuhan kami, ampunilah dosa-dosa kami dan dosa kedua orang tua kami, dan sayangilah mereka sebagaimana mereka mendidik kami di waktu kecil. Wahai Tuhan kami, berikanlah kami kebaikan di dunia dan kebaikan di akhirat, dan lindungilah kami dari azab siksa api neraka. Dan semoga Allah melimpahkan rahmat dan keselamatan kepada junjungan kami Nabi Muhammad, keluarga, dan para sahabatnya. Segala puji bagi Allah, Tuhan semesta alam."
+    }
+};
